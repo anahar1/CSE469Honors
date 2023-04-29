04333507c6a00be7cd2818b00a372024be3573aa
jolak1 <jolakken@asu.edu>
2023-04-29 13:58:10 -0700

    initialPlans

--- FinalMain.py
+++ FinalMain.py
@@ -1,5 +1,8 @@
+from finalMethods import *
+import sys
 
 #user profiles = [Number of followers, number of followings, is_verified, number_of_posts, has_profile_picture, created_recently]
+#scraped data from instagram public profiles for academic use
 userProfiles = {}
 userProfiles["cristiano"] = [579384173, 557, True, 3477, True]
 userProfiles["asucatholic"] = [1908, 108, False, 336, True]
@@ -10,6 +13,24 @@ userProfiles["cale"] = [690, 238, False, 703, True]
 userProfiles["krabby"] = [0, 23, False, 21, True]
 userProfiles["slaws"] = [2, 0, False, 0, False]
 userProfiles["kane"] = [33600, 975, False, 491, True]
+
+#Get the account to run our algorithm with
+print("Enter the Instagram Account ID: ", end="")
+instagramID = input()
+
+#check if user is verified and if the user is verified, print verification status and do sys.exit()
+
+#percent chance the passed in profile is a bot
+botChance = 0
+
+#Computes follower to following ratio
+userRatio = ffRatio(userProfiles[instagramID][0], userProfiles[instagramID][1])
+
+if(userRatio < 2):
+    botChance += 25
+
+
+print("The percent chance the profile is a bot is: ", botChance, "%")
 
 
 
---
+++ finalMethods.py
@@ -0,0 +1,4 @@
+def ffRatio(followers, followings):
+    ratio = followers/followings
+    print("The follower to following ratio is: ", ratio)
+    return ratio
