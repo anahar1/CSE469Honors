d920f538843f95db6eb34ca964c703faf72afffb
jolak1 <jolakken@asu.edu>
2023-04-29 13:39:53 -0700

    Update FinalMain.py

---
+++ FinalMain.py
@@ -0,0 +1,18 @@
+
+#user profiles = [Number of followers, number of followings, is_verified, number_of_posts, has_profile_picture, created_recently]
+userProfiles = {}
+userProfiles["cristiano"] = [579384173, 557, True, 3477, True]
+userProfiles["asucatholic"] = [1908, 108, False, 336, True]
+userProfiles["kyoumb"] = [1, 1, False, 3477, False]
+userProfiles["katara"] = [59100, 0, False, 562, True]
+userProfiles["zan3"] = [1683, 41, False, 0, False]
+userProfiles["cale"] = [690, 238, False, 703, True]
+userProfiles["krabby"] = [0, 23, False, 21, True]
+userProfiles["slaws"] = [2, 0, False, 0, False]
+userProfiles["kane"] = [33600, 975, False, 491, True]
+
+
+
+
+
+
